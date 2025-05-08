47b0afb7e0ba8d0c02abc504f4a6a67421c6160b
Gabriel Barbosa <gb119m@gmail.com>
2025-05-07 21:39:55 -0300

    Update User.java

--- login-auth-api/src/main/java/com/example/login_auth_api/domain/user/User.java
+++ login-auth-api/src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -1,26 +1,16 @@
 package com.example.login_auth_api.domain.user;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Entity
-@Table(name = "tb_users")
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
+
+    private Long id;
     private String email;
     private String name;
     private String password;
 
-    @Enumerated(EnumType.STRING)
     private UserRole role;
+
+    public User() {}
+
+
 }
