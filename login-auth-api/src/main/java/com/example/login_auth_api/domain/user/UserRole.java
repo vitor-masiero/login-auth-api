eb7d0b5e70ba0ae4ef154862d2b61b6395d85e06
José Vitor <masierojosevitor456@gmail.com>
2026-03-18 23:36:03 -0300

    qa - organizando projeto

--- login-auth-api/src/main/java/com/example/login_auth_api/domain/user/UserRole.java
+++
@@ -1,8 +0,0 @@
-package com.example.login_auth_api.domain.user;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum UserRole {
-    @JsonProperty("USER") ROLE_USER,
-    @JsonProperty("ADMIN") ROLE_ADMIN
-}
--- login-auth-api/src/main/java/com/example/login_auth_api/exceptions/InvalidUserException.java
+++
@@ -1,7 +0,0 @@
-package com.example.login_auth_api.exceptions;
-
-public class InvalidUserException extends RuntimeException {
-    public InvalidUserException(String message) {
-        super(message);
-    }
-}
